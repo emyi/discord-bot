0f3ea85ac5583dc257d9c7777ec5295051337aa1
emyi <trend1987@gmail.com>
2017-12-09 18:37:34 -0800

    fixed msg

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-	let healers = msg.split( '-' )[1];
-	let tanks = msg.split( '-' )[2];
-	let dps = msg.split( '-' )[3];
-	let purpose = msg.split( '-' )[4];
+	let healers = msg.content.split( '-' )[1];
+	let tanks = msg.content.split( '-' )[2];
+	let dps = msg.content.split( '-' )[3];
+	let purpose = msg.content.split( '-' )[4];
 	//Check if command begins with createGroup
 	if( msg.content.split( '-' )[0] == 'createGroup' ) {
 		//Checks if number inputs are numbers
